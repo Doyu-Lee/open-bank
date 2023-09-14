bc51b9427f8f36d88353f46073527894608f2ea9
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-15 03:04:39 +0900

    chore: CORS Proxy 설정

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/v2.0/:path*',
+        destination: 'https://testapi.openbanking.or.kr/v2.0/:path*',
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

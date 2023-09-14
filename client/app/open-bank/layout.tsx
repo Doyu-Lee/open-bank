9457ff5c84fb987fa9788b26f09d50b93921fca1
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-15 02:48:55 +0900

    feat: 사용자 인증 화면으로 이동하는 페이지

--- client/app/open-bank/layout.tsx
+++ client/app/open-bank/layout.tsx
@@ -1,15 +1,12 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
 
-const Layout = ({children}: {
-  children: React.ReactNode
-}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <div className={styles.mainTitled}>테스트</div>
+    <div>
+      <div className={styles.mainTitle}>오픈뱅킹 테스트</div>
       {children}
-    </>
-    
-  )
-}
+    </div>
+  );
+};
 
 export default Layout;
--- client/app/open-bank/page.tsx
+++ client/app/open-bank/page.tsx
@@ -1,9 +1,27 @@
-import styles from './page.module.css'
+'use client';
+
+import styles from './page.module.css';
 
 const OpenBank = () => {
+  const redirectURL = process.env.NEXT_PUBLIC_BANK_REDIRECT_URI;
+  const clientId = process.env.NEXT_PUBLIC_BANK_CLIENT_ID;
+  const scope = process.env.NEXT_PUBLIC_SCOPE;
+  const state = process.env.NEXT_PUBLIC_STATE;
+  const authType = process.env.NEXT_PUBLIC_AUTH_TYPE;
+
+  const authURL = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scope}&state=${state}&auth_type=${authType}`;
+
+  const handleOAuthLogin = () => {
+    window.location.href = authURL;
+  };
+
   return (
-    <div className={styles.mainTitled}>테스트</div>
-  )
-}
+    <section className={styles.authPage}>
+      <button type='button' onClick={handleOAuthLogin} className={styles.authBtn}>
+        사용자 인증하기
+      </button>
+    </section>
+  );
+};
 
 export default OpenBank;
